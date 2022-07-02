f5b1dd8226def586cb900208d1b33400299d5645
aayahda <aadhyakraj@gmail.com>
2022-07-02 19:04:41 +0530

    splash screen 1

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/SplashScreen1.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen1(),
+    );
+  }
+}
