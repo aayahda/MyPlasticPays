4fa843a130512d60f92db8c99ec7450a97d61ef9
aayahda <aadhyakraj@gmail.com>
2022-07-04 17:39:31 +0530

    rewards screen

--- lib/Screens/RewardsScreen.dart
+++ lib/Screens/RewardsScreen.dart
@@ -98,7 +98,83 @@ class _RewardsScreenState extends State<RewardsScreen> {
                           )
                         ],
                       ),
-                    ))
+                    )),
+                  SizedBox(height: 10,),
+                  Container(height: 70,width:double.infinity,decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)),color: Color(0xffFAF4FF)),
+                      child:Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text("5"),
+                            Padding(
+                              padding: const EdgeInsets.only(left:12,right: 12),
+                              child: CircleAvatar(radius: 30,),
+                            ),
+                            Expanded(child:Text("Name")),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("869"),
+                            )
+                          ],
+                        ),
+                      )),
+                  SizedBox(height: 10,),
+                  Container(height: 70,width:double.infinity,decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)),color: Color(0xffFAF4FF)),
+                      child:Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text("6"),
+                            Padding(
+                              padding: const EdgeInsets.only(left:12,right: 12),
+                              child: CircleAvatar(radius: 30,),
+                            ),
+                            Expanded(child:Text("Name")),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("869"),
+                            )
+                          ],
+                        ),
+                      )),
+                  SizedBox(height: 10,),
+                  Container(height: 70,width:double.infinity,decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)),color: Color(0xffFAF4FF)),
+                      child:Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text("7"),
+                            Padding(
+                              padding: const EdgeInsets.only(left:12,right: 12),
+                              child: CircleAvatar(radius: 30,),
+                            ),
+                            Expanded(child:Text("Name")),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("869"),
+                            )
+                          ],
+                        ),
+                      )),
+                  SizedBox(height: 10,),
+                  Container(height: 70,width:double.infinity,decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)),color: Color(0xffFAF4FF)),
+                      child:Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text("9"),
+                            Padding(
+                              padding: const EdgeInsets.only(left:12,right: 12),
+                              child: CircleAvatar(radius: 30,),
+                            ),
+                            Expanded(child:Text("Name")),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("869"),
+                            )
+                          ],
+                        ),
+                      )),
                 ],
               ),
             ),
