ad261e8d743fc5e98b8cb760bc422e01f22cfcf0
aayahda <aadhyakraj@gmail.com>
2022-07-06 22:10:26 +0530

    location screen

--- lib/Screens/BinLocation.dart
+++ lib/Screens/BinLocation.dart
@@ -11,7 +11,38 @@ class _BinLocationState extends State<BinLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0,left: 10),
+                  child: Text("Welcome Aadhya,",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.black,fontSize: 16,fontStyle: FontStyle.italic),),
+                ),
+                Text("30 Points",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 30,color: Colors.black),),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10,top: 10,right: 140,bottom: 10),
+              child: Text("This week's reward is sponsored by ",style:TextStyle(fontStyle: FontStyle.italic,color: Colors.black45)),
+            ),
+            Container(margin: EdgeInsets.all(10),
+                height: 100,width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
+                  boxShadow: kElevationToShadow[9],
+                ),
+              child: Image.asset("Assets/Amal.jpeg"),
+            ),
+          ],
+
+        ),
+      ),
     );
   }
 }
