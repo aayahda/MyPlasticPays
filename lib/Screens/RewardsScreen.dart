704a0a02f444017892c00b45fa0d426be85ef863
aayahda <aadhyakraj@gmail.com>
2022-07-06 20:11:10 +0530

    rewards screen

--- lib/Screens/RewardsScreen.dart
+++ lib/Screens/RewardsScreen.dart
@@ -13,106 +13,52 @@ class _RewardsScreenState extends State<RewardsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(top: 50,left: 20,right: 20),
+        padding: const EdgeInsets.only(top: 50,left: 10,right: 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Your Balance",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300),),
-            Text("30 Points",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 30,color: Colors.black),),
-            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text("Your Balance",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text("30 Points",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 30,color: Colors.black),),
+            ),
+            // SizedBox(height: 20,),
             Expanded(
               child: ListView(
                 children: [
-                  Container(height: 120,width: double.infinity,
-                      decoration: BoxDecoration(
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0),
+                    child: Container(margin: EdgeInsets.all(10),
+                        height: 120,width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
+                        boxShadow: kElevationToShadow[9],
+                    )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 6,bottom: 6),
+                    child: Container(margin: EdgeInsets.all(10),
+                        height: 120,width: double.infinity,
+                        decoration: BoxDecoration(
                           color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
-                    boxShadow: [
-                    // color: Colors.white, //background color of box
-                    BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 30.0, // soften the shadow
-                      spreadRadius: 1.0, //extend the shadow
-                      offset: Offset(
-                        0, // Move to right 10  horizontally
-                        10, // Move to bottom 10 Vertically
-                      ),
-                    )
-                  ],)),
-                  Container(height: 120,width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
-                        boxShadow: [
-                          // color: Colors.white, //background color of box
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 50.0, // soften the shadow
-                            spreadRadius: 1.0, //extend the shadow
-                            offset: Offset(
-                              15, // Move to right 10  horizontally
-                              15, // Move to bottom 10 Vertically
-                            ),
-                          )
-                        ],)),
-                  Container(height: 120,width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
-                        boxShadow: [
-                          // color: Colors.white, //background color of box
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 50.0, // soften the shadow
-                            spreadRadius: 1.0, //extend the shadow
-                            offset: Offset(
-                              15, // Move to right 10  horizontally
-                              15, // Move to bottom 10 Vertically
-                            ),
-                          )
-                        ],)),
-                  Container(height: 120,width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
-                        boxShadow: [
-                          // color: Colors.white, //background color of box
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 50.0, // soften the shadow
-                            spreadRadius: 1.0, //extend the shadow
-                            offset: Offset(
-                              15, // Move to right 10  horizontally
-                              15, // Move to bottom 10 Vertically
-                            ),
-                          )
-                        ],)),
-                  Container(height: 120,width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
-                        boxShadow: [
-                          // color: Colors.white, //background color of box
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 50.0, // soften the shadow
-                            spreadRadius: 1.0, //extend the shadow
-                            offset: Offset(
-                              15, // Move to right 10  horizontally
-                              15, // Move to bottom 10 Vertically
-                            ),
-                          )
-                        ],)),
-                  Container(height: 120,width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
-                        boxShadow: [
-                          // color: Colors.white, //background color of box
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 50.0, // soften the shadow
-                            spreadRadius: 1.0, //extend the shadow
-                            offset: Offset(
-                              15, // Move to right 10  horizontally
-                              15, // Move to bottom 10 Vertically
-                            ),
-                          )
-                        ],)),
+                          boxShadow: kElevationToShadow[9],
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0),
+                    child: Container(margin: EdgeInsets.all(10),
+                        height: 120,width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15)),
+                          boxShadow: kElevationToShadow[9],
+                        )),
+                  ),
+
+
+
                 ],
               ),
             )
